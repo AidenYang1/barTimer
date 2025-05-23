53590399f12daa00105e68def88778196a3d4b85
TelePuryang <60610023+TelePuryang@users.noreply.github.com>
2025-05-23 11:48:14 +0800

    Initial Commit

---
+++ barTimer/barTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barTimerApp.swift
+//  barTimer
+//
+//  Created by 杨翼臣 on 2025/5/23.
+//
+
+import SwiftUI
+
+@main
+struct barTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ barTimerTests/barTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  barTimerTests.swift
+//  barTimerTests
+//
+//  Created by 杨翼臣 on 2025/5/23.
+//
+
+import Testing
+@testable import barTimer
+
+struct barTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
